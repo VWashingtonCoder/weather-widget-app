d83108bf23342620609ec71e2889711a16353053
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-04-10 13:01:56 -0600

    Clean up api data

--- src/components/ForecastContainer.jsx
+++ src/components/ForecastContainer.jsx
@@ -7,8 +7,22 @@ class ForecastContainer extends React.Component {
   async componentDidMount() {
     try {
         const response = await fetch(`${WEATHER_URL}${WEATHER_API}`);
-        const json = await response.json();
-        console.log(json);
+        if (response.ok) {
+            // console.log(response);
+            const json = await response.json();
+            // console.log(json);
+            const data = json.list.filter(day => day.dt_txt.includes("00:00:00")).map(item => ({
+                temp: item.main.temp,
+                dt: item.dt,
+                date: item.dt_txt,
+                imgID: item.weather[0].icon,
+                desc: item.weather[0].description
+            }));
+            console.log(data);
+        } else {
+            // Do Something
+        }
+        
     } catch(err) {
         console.error("There was an error: ", err);
     }
