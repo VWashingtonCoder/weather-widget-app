8a6dd342bccb22d26d3975e04ae0e076f8358189
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-04-10 12:50:52 -0600

    Set up api call function

--- src/components/ForecastContainer.jsx
+++ src/components/ForecastContainer.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import DayCard from "./DayCard";
 import DegreeToggle from "./DegreeToggle";
+import { WEATHER_API, WEATHER_URL } from "../constants";
 
 class ForecastContainer extends React.Component {
-    render() {
-        return (
-            <>
-                <div>Forecast Container</div>
-                <DegreeToggle />
-                <DayCard />
-            </>
-        )
+  async componentDidMount() {
+    try {
+        const response = await fetch(`${WEATHER_URL}${WEATHER_API}`);
+        const json = await response.json();
+        console.log(json);
+    } catch(err) {
+        console.error("There was an error: ", err);
     }
-};
+  }
+
+  render() {
+    return (
+      <>
+        <div>Forecast Container</div>
+        <DegreeToggle />
+        <DayCard />
+      </>
+    );
+  }
+}
 
 export default ForecastContainer;
