bc9451b2d2bfe67c1c621b5986282e1b9ab66b6e
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-04-10 13:15:41 -0600

    Set data to this state

--- src/components/ForecastContainer.jsx
+++ src/components/ForecastContainer.jsx
@@ -4,36 +4,51 @@ import DegreeToggle from "./DegreeToggle";
 import { WEATHER_API, WEATHER_URL } from "../constants";
 
 class ForecastContainer extends React.Component {
+  state = {
+    data: [],
+    loading: false,
+    error: false,
+  };
   async componentDidMount() {
+    this.setState(({ loading: true }))
     try {
-        const response = await fetch(`${WEATHER_URL}${WEATHER_API}`);
-        if (response.ok) {
-            // console.log(response);
-            const json = await response.json();
-            // console.log(json);
-            const data = json.list.filter(day => day.dt_txt.includes("00:00:00")).map(item => ({
-                temp: item.main.temp,
-                dt: item.dt,
-                date: item.dt_txt,
-                imgID: item.weather[0].icon,
-                desc: item.weather[0].description
-            }));
-            console.log(data);
-        } else {
-            // Do Something
-        }
-        
-    } catch(err) {
-        console.error("There was an error: ", err);
+      const response = await fetch(`${WEATHER_URL}${WEATHER_API}`);
+      if (response.ok) {
+        const json = await response.json();
+        const data = json.list
+          .filter((day) => day.dt_txt.includes("00:00:00"))
+          .map((item) => ({
+            temp: item.main.temp,
+            dt: item.dt,
+            date: item.dt_txt,
+            imgID: item.weather[0].icon,
+            desc: item.weather[0].description,
+          }));
+        this.setState(({ 
+            data: data,
+            loading: false,
+        }));
+      } else {
+        this.setState(({
+            loading: false,
+            error: true,
+        }))
+      }
+    } catch (err) {
+      console.error("There was an error: ", err);
     }
   }
 
   render() {
+    const { loading, error, data } = this.state;
     return (
       <>
         <div>Forecast Container</div>
         <DegreeToggle />
-        <DayCard />
+        {!loading 
+            ? data.map((item) => (<DayCard />)) 
+            : <div>Loading...</div>
+        }
       </>
     );
   }
