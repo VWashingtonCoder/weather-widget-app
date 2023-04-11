dd7c7b72c461c051640dba1deda6edcc0bd25703
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-04-11 11:53:07 -0600

    Move api call to services

--- src/components/ForecastContainer.jsx
+++ src/components/ForecastContainer.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import DayCard from "./DayCard";
 import DegreeToggle from "./DegreeToggle";
 import { WEATHER_API, WEATHER_URL } from "../constants";
+import WeatherService from "../services";
+
+const weather = new WeatherService();
 
 class ForecastContainer extends React.Component {
   state = {
@@ -11,37 +14,25 @@ class ForecastContainer extends React.Component {
     degreeType: "fahrenheit",
     speedType: "mph"
   };
-  async componentDidMount() {
+  
+  componentDidMount() {
     this.setState(({ loading: true }))
-    try {
-      const response = await fetch(`${WEATHER_URL}${WEATHER_API}`);
-      if (response.ok) {
-        const json = await response.json();
-        const data = json.list
-          .filter((day) => day.dt_txt.includes("00:00:00"))
-          .map((item) => ({
-            temp: item.main.temp,
-            dt: item.dt,
-            date: item.dt_txt,
-            imgId: item.weather[0].id,
-            desc: item.weather[0].description,
-            feelsLike: item.main.feels_like,
-            humidity: item.main.humidity,
-            windSpeed:item.wind.speed
-          }));
-        this.setState(({ 
-            data: data,
-            loading: false,
-        }));
-      } else {
+    weather.fetchFiveDayForecast().then((res) => {
+        if (res && res.resp.ok) {
+            this.setState(({ 
+                data: res.data,
+                loading: false,
+            }));
+        } else {
+            this.setState(({ loading: false }));
+        }
+    }, (error) => {
+        console.log(error);
         this.setState(({
             loading: false,
             error: true,
-        }))
-      }
-    } catch (err) {
-      console.error("There was an error: ", err);
-    }
+        }));
+    })
   }
   updateForecastDegree = ({ target: { value } }) => {
     this.setState(({ degreeType: value }));
---
+++ src/services.js
@@ -0,0 +1,33 @@
+import { WEATHER_URL, WEATHER_API } from "./constants";
+
+class WeatherService {
+  async fetchFiveDayForecast() {
+    return new Promise(async (success, fail) => {
+      try {
+        const resp = await fetch(`${WEATHER_URL}${WEATHER_API}`);
+        if (resp.ok) {
+          const json = await resp.json();
+          const data = json.list
+            .filter((day) => day.dt_txt.includes("00:00:00"))
+            .map((item) => ({
+              temp: item.main.temp,
+              dt: item.dt,
+              date: item.dt_txt,
+              imgId: item.weather[0].id,
+              desc: item.weather[0].description,
+              feelsLike: item.main.feels_like,
+              humidity: item.main.humidity,
+              windSpeed: item.wind.speed,
+            }));
+            success({ resp, data });
+        } else {
+            fail({ error: "Invalid http request" });
+        }
+      } catch (err) {
+        fail(err);
+      }
+    });
+  }
+}
+
+export default WeatherService;
