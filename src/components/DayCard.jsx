c65d3291fc7c2357a7801d08b45f7c44ef435f24
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-04-11 11:36:40 -0600

    Add in more fields and data displays

--- src/components/DayCard.jsx
+++ src/components/DayCard.jsx
@@ -2,29 +2,27 @@ import React from "react";
 import moment from "moment";
 
 const DayCard = ({ data }) => {
-    const { temp, dt, imgId, desc } = data;
+  const { temp, dt, imgId, desc } = data;
 
-    const newDate = new Date();
-    newDate.setTime(dt * 1000);
-    const icon = `owf owf-${imgId} owf-5x`;
+  const newDate = new Date();
+  newDate.setTime(dt * 1000);
+  const icon = `owf owf-${imgId} owf-5x`;
 
-    return (
-        <div className="col-sm-2">
-            <div className="card">
-                <h3 className="card-title">
-                    {moment(newDate).format("dddd")}
-                </h3>
-                <p className="text-muted">
-                    {moment(newDate).format("MMMM Do, h:mm a")}
-                </p>
-                <i className={icon}></i>
-                <h2>{Math.round(temp)} °F</h2>
-                <div className="card-body">
-                    <p className="card-text">{desc}</p>
-                </div>
-            </div>
+  return (
+    <div className="col-sm-2">
+      <div className="card">
+        <h3 className="card-title">{moment(newDate).format("dddd")}</h3>
+        <p className="text-muted">
+          {moment(newDate).format("MMMM Do, h:mm a")}
+        </p>
+        <i className={icon}></i>
+        <h2>{Math.round(temp)} °F</h2>
+        <div className="card-body">
+          <p className="card-text">{desc}</p>
         </div>
-    )
+      </div>
+    </div>
+  );
 };
 
 export default DayCard;
